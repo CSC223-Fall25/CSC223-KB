fc46a778f3616d908185eb2e247455fdd3c277de
Kelvin Bonsu <kelvinjnr196@gmail.com>
2025-04-16 10:29:08 -0400

    Done code for Path Exist

--- app/src/main/java/PathExist.java
+++ app/src/main/java/PathExist.java
@@ -1,37 +1,47 @@
 public class PathExist {
-    // This fills the adjacency list with the edges
-    for(int i = 0; i < edges.length; i++) {
-        int node1 = edges[i][0];
-        int node2 = edges[i][1];
 
-        graph.get(node1).add(node2);
-        graph.get(node2).add(node1);
-    } 
-    boolean[] visited = new boolean[n];// This created a boolean array to keep track of nodes that have already been visited
-    Queue<Integer> queue = new LinkedList<>(); // This creates a queue for BFS
-   
-    // This adds th source node to the queue and it marks it as visited
-    queue.add(source);
-    visited[source] = true;
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        boolean[][] graph = new boolean[n][n];
 
-   // This starts the BFS loop
-    while(!queue.isEmpty()) { 
-        int curr = queue.poll();
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
 
-        if(curr == destination) { // If the curr node is the destination, we reutrn true
+            // connecting both directions
+            graph[u][v] = true;
+            graph[v][u] = true;
+        }
+
+        // array to track which nodes we've already checked
+        boolean[] visited = new boolean[n];
+
+        // Start DFS from the source node
+        return DFS(source, destination, graph, visited);
+    }
+
+    public boolean DFS(int current, int destination, boolean[][] graph, boolean[] visited) {
+        // If we reach the destination, return true
+        if (current == destination) {
             return true;
         }
-    }
-    // Goes through all the neighbors of the curr node
-    List<Integer> neighbors = graph.get(curr);
-    for(int i = 0; i< neighbors.size(); i++) {
-        int neighbor = neighbors.get(i);
 
-        // This is if we have not visited the neighbor yet
-        if(!visited[neighbor]) {
-            queue.add(neighbor);
-            visited[neighbor] = true;
+        // Mark the current node as visited
+        visited[current] = true;
+
+        // Try all possible neighbors
+        for (int i = 0; i < graph.length; i++) {
+            // Check if there's a connection and the neighbor hasn't been visited
+            if (graph[current][i] && !visited[i]) {
+
+                boolean found = DFS(i, destination, graph, visited);
+               
+                if (found) {
+                    return true;
+                }
+            }
         }
-        return false; // If we finish the BFS and we did not find the destination, return false
+
+        // If no path was found, return false
+        return false;
     }
 }
